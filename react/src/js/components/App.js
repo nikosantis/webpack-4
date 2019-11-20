9049e43b0a115fcd7c6988ab988766d326343ace
Nikolas Santis <nikosantis@gmail.com>
2019-11-19 21:42:06 -0300

    React

---
+++ react/src/js/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Que linda Aplicación hecha en React.js
+    </div>
+  )
+}
+
+export default App
---
+++ react/src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/index.css'
+
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App'
+
+render(<App />, document.getElementById('container'))
